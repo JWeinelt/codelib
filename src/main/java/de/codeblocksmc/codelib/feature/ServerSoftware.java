6bea9686de73e9f6c86aedc420171b17207b21ac
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-27 15:42:40 +0200

    Overview of server implementations

---
+++ src/main/java/de/codeblocksmc/codelib/feature/ServerSoftware.java
@@ -0,0 +1,12 @@
+package de.codeblocksmc.codelib.feature;
+
+public enum ServerSoftware {
+    BUNGEECORD,
+    WATERFALL,
+    VELOCITY,
+    SPIGOTMC,
+    CRAFT_BUKKIT,
+    PAPER,
+    PURPUR,
+    FOLIA
+}
