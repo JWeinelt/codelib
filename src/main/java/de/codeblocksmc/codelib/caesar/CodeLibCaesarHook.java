0e818ba187a5b9feb5ed07d570d33413e65d41c9
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-27 15:43:18 +0200

    hooking for Caesar

---
+++ src/main/java/de/codeblocksmc/codelib/caesar/CodeLibCaesarHook.java
@@ -0,0 +1,30 @@
+package de.codeblocksmc.codelib.caesar;
+
+import de.julianweinelt.caesar.plugin.CPlugin;
+
+public class CodeLibCaesarHook extends CPlugin {
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onDefineEvents() {
+
+    }
+
+    @Override
+    public void onCreateCommands() {
+
+    }
+}
